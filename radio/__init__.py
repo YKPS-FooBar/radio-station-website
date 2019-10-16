cb57acda8d1a3f8146fddc939488ed242e4c3655
David Ma <davidmaamoaix@gmail.com>
2019-10-16 15:18:28 +0800

    Database model

--- radio/__init__.py
+++ radio/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+
+db = SQLAlchemy(app)
+db.app = app
+db.init_app(app)
 
 
 import radio.views
---
+++ radio/models.py
@@ -0,0 +1,21 @@
+from radio import db
+
+
+class Song(db.Model):
+	'''The database for songs.'''
+
+	__tablename__ = 'songs'
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(80), nullable=False)
+	start_time = db.Column(db.DateTime, nullable=False)
+	file_name = db.Column(db.String(80), nullable=True)
+
+
+	def __init__(self, name, start_time, file_name=None):
+		self.name = name
+		self.start_time = start_time
+		self.file_name = file_name
+
+
+db.create_all()
